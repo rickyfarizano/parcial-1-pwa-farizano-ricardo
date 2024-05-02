78357d51baec36d31a8e79c5e07fd7f922ff0257
rickyfarizano <rickyfarizano@gmail.com>
2024-05-02 15:38:52 -0300

    feat: agregado de logica para mostrar los detalles ampliados de cada pokemon

--- js/details.js
+++ js/details.js
@@ -1,3 +1,72 @@
-// const name_pokemon = window.location.href.split('?name=')[1];
+const pokemonId = window.location.href.split("?id=")[1];
+const URL_ENDPOINT = "https://pokeapi.co/api/v2/";
+const URL_POKEMON = URL_ENDPOINT + "pokemon/" + pokemonId;
 
-// console.log(name_pokemon);
+/** 
+ * Me genera las cards para cada Pokemon.
+ * @param pokemon Un objeto que contenga los datos de tu pokemon.
+ */
+const ShowCard = (pokemon) => {
+    const contenedor = document.querySelector("#container-list");
+    const ul = document.createElement("ul");
+    ul.classList.add("list-details");
+    
+    const img = document.createElement("img");
+    img.src = pokemon.sprites.front_default;
+    img.alt = pokemon.name + " sprite";
+
+    const h3 = document.createElement("h3");
+    const li = document.createElement("li");
+    li.classList.add("list-item");
+
+    const p = document.createElement("p");
+    p.innerText = "Tipo:" + pokemon.types.map(type => type.type.name).join(", ");
+    p.classList.add("text-list");
+
+    // Creo las habilidades
+    const abilitiesList = document.createElement("ul");
+    abilitiesList.classList.add("habilities");
+
+    const abilityListTitle = document.createElement('h4');
+    abilityListTitle.classList.add("ability-title");
+    abilityListTitle.innerText = "Habilidades";
+    abilitiesList.append(abilityListTitle);
+
+    // Creo las estadisticas
+    const statsList = document.createElement("ul");
+    statsList.classList.add("habilities");
+
+    const statsListTitle = document.createElement('h4');
+    statsListTitle.classList.add("ability-title");
+    statsListTitle.innerText = "Estadísticas";
+    statsList.append(statsListTitle);
+
+    // Recorro las habilidades y las agrego a la lista
+    pokemon.abilities.forEach(ability => {
+        let li = document.createElement("li");
+        li.innerText = ability.ability.name;
+
+        abilitiesList.append(li);
+    });
+
+    // Recorro las estadisticas y las agrego a la lista
+    pokemon.stats.forEach(stat => {
+        let li = document.createElement("li");
+        li.innerText = `Stat: ${stat.base_stat} `;
+        li.innerText += `Effort: ${stat.effort}`;
+
+        statsList.append(li);
+    });
+    
+    h3.innerText = pokemon.name;
+    li.append(h3, p);
+    ul.append(img, li, abilitiesList, statsList);
+    contenedor.appendChild(ul);
+}
+
+fetch(URL_POKEMON)
+.then(data => data.json())
+.then(pokemons => {
+    console.log(pokemons);
+    ShowCard(pokemons);
+});
--- js/main.js
+++ js/main.js
@@ -4,11 +4,15 @@ const limit = 100;
 const offset = 0;
 const URL_LIMIT = `${URL_POKEMON}?limit=${limit}&offset=${offset}`;
 
+/** 
+ * Me genera las cards para cada Pokemon.
+ * @param pokemon Un objeto que contenga los datos de tu pokemon.
+ */
 const ShowCard = (pokemon) => {
     const contenedor = document.querySelector("#container-list");
     const ul = document.createElement("ul");
     ul.classList.add("list-details");
-
+    
     const img = document.createElement("img");
     img.src = pokemon.sprites.front_default;
     img.alt = pokemon.name + " sprite";
@@ -21,20 +25,34 @@ const ShowCard = (pokemon) => {
     p.innerText = "Tipo:" + pokemon.types.map(type => type.type.name).join(", ");
     p.classList.add("text-list");
 
+    const btn = document.createElement("button");
+    btn.classList.add("btn-card");
+    btn.innerText = "Ver mas";
+
+    btn.addEventListener("click", () => {
+        ShowDetails(pokemon.id);
+    });
+
     h3.innerText = pokemon.name;
     li.append(h3, p);
-    ul.append(img, li);
+    ul.append(img, li, btn);
     contenedor.appendChild(ul);
+}
+
+/**
+ * Muestra los detalles maximizados de cada pokemon.
+ * @param id Numero identificador o nombre del pokemon.
+ */
+const ShowDetails = (id) => {
+    window.location.href = "http://127.0.0.1:5500/details.html?id=" + id;
 }
 
 fetch(URL_LIMIT)
 .then(data => data.json())
 .then(pokemons => {
     const pokemonList = pokemons.results;
-    let pokemonCount;
 
     pokemonList.forEach(pokemon => {
-        console.log(pokemon);
         // Obtengo el id de cada pokemon
         const pokemonId = pokemon.url.split("/")[6];
         
