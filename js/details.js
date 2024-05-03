87fcdd2518d996e5f74db1f215aa113b684aaba1
rickyfarizano <rickyfarizano@gmail.com>
2024-05-03 14:48:02 -0300

    estilos detalles pokemon

--- js/details.js
+++ js/details.js
@@ -1,13 +1,14 @@
 const pokemonId = window.location.href.split("?id=")[1];
 const URL_ENDPOINT = "https://pokeapi.co/api/v2/";
 const URL_POKEMON = URL_ENDPOINT + "pokemon/" + pokemonId;
-const URL_POKEMON_CHARACT = URL_ENDPOINT + "characteristic/" + pokemonId; 
+const URL_POKEMON_CHARACT = URL_ENDPOINT + "pokemon-species/" + pokemonId; 
 
 /** 
  * Me genera las cards para cada Pokemon.
  * @param pokemon Un objeto que contenga los datos de tu pokemon.
+ * @param characteristics Un objeto que contenga las caracteristicas de tu pokemon.
  */
-const ShowCard = (pokemon) => {
+const ShowCard = (pokemon, characteristics) => {
     const contenedor = document.querySelector("#container-list");
     const ul = document.createElement("ul");
     ul.classList.add("list-details-extended");
@@ -22,6 +23,16 @@ const ShowCard = (pokemon) => {
 
     const h3 = document.createElement("h3");
     h3.classList.add("pokemon-name-details");
+
+    //Numero del pokemon
+    const number = document.createElement("p");
+    number.classList.add("number-details");
+    number.innerText = `Numero: ${characteristics.order}`;
+
+    // Ratio de captura
+    const captureRatio = document.createElement("p");
+    captureRatio.classList.add("ratio-details");
+    captureRatio.innerText = `Ratio de captura: ${characteristics.capture_rate}`;
 
     const li = document.createElement("li");
     li.classList.add("list-item");
@@ -68,13 +79,16 @@ const ShowCard = (pokemon) => {
     h3.innerText = pokemon.name;
     li.append(h3, p);
     containerListDetails.append( abilitiesList, statsList);
-    ul.append(img, li, containerListDetails);
+    ul.append(img, li, number, captureRatio, containerListDetails);
     contenedor.appendChild(ul);
 }
 
 fetch(URL_POKEMON)
 .then(data => data.json())
 .then(pokemons => {
-    console.log(pokemons);
-    ShowCard(pokemons);
+    fetch(URL_POKEMON_CHARACT)
+    .then(data => data.json())
+    .then(characteristics => {
+        ShowCard(pokemons, characteristics);
+    });
 });
